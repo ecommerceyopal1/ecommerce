3d57e698012c19815d40c8de87120cd75dab8944
ecommerceyopal1 <ecommerceyopal1@gmail.com>
2024-04-17 12:10:48 -0500

    Agregados archivos de la carpeta ecommerce

---
+++ Config/Config.php
@@ -0,0 +1,11 @@
+<?php
+const BASE_URL = "http://localhost/ecommerce/";
+const HOST = "localhost";
+const USER = "root";
+const PASS = "";
+const DB = "bdecommerce";
+const CHARSET = "charset=utf8";
+const TITLE = "ECOMMERCE";
+const MONEDA = "USD";
+const CLIENT_ID = "AZf7-Yp1dBk-xABjec-zTciM70RuatIlpC6s5Zqxy6zxrTVKX-wcDWCvEPsBGzK1zHjoF8UAuhRGWClb";
+?>
